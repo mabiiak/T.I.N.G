195133be0b2967b9f88c5632e67c54d45ab395a5
mabiiak <mabiane.polniak@gmail.com>
2022-09-17 15:52:07 -0300

    feat: implement function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+from os.path import exists
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if '.txt' not in path_file:
+        print(f"Formato inválido", file=sys.stderr)
+        return None
+
+    if not exists(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
+
+    with open(path_file, "r") as file:
+        return file.read().split("\n")
+
+
+# https://www.pythontutorial.net/python-basics/python-check-if-file-exists/
