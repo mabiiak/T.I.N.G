d197d31323181b6331a73d5f9b11f5703bf46c7c
mabiiak <mabiane.polniak@gmail.com>
2022-09-19 18:33:42 -0300

    feat: implement function exist_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = instance._data
+
+    result = []
+    ocorrencias = []
+
+    for i, palavra in enumerate(data[0]["linhas_do_arquivo"]):
+        if word.lower() in palavra.lower():
+            ocorrencias.append({ "linha": i + 1 })
+
+    if len(ocorrencias) != 0:
+        result.append({
+            "palavra": word,
+            "arquivo": data[0]["nome_do_arquivo"],
+            "ocorrencias": ocorrencias
+        })
+    
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
