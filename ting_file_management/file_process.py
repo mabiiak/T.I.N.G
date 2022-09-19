8723bde008c7b6b40f76705eac84ceb569bf1c73
mabiiak <mabiane.polniak@gmail.com>
2022-09-19 14:16:46 -0300

    fix: lint

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,11 +1,12 @@
 import sys
-from ting_file_management.file_management import txt_importer 
+from ting_file_management.file_management import txt_importer
+
 
 def process(path_file, instance):
     for i in range(len(instance)):
         if instance.search(i)["nome_do_arquivo"] == path_file:
             return None
-    
+
     texto = txt_importer(path_file)
 
     data = {
