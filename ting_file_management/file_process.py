8db0534bd4f64721e191ad6dd359495ef83ee64c
mabiiak <mabiane.polniak@gmail.com>
2022-09-19 09:13:34 -0300

    feat: implement function to add file to queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer 
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    texto = txt_importer(path_file)
+
+    instance.enqueue(texto)
+
+    formato = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(texto),
+        "linhas_do_arquivo": instance._data[-1],
+    }
+
+    print(formato)
 
 
 def remove(instance):
