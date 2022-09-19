d9d57c7a38c20b011a6416528098f8dfda235d56
mabiiak <mabiane.polniak@gmail.com>
2022-09-19 18:36:27 -0300

    fix: identation in js

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -6,7 +6,7 @@ def exists_word(word, instance):
 
     for i, palavra in enumerate(data[0]["linhas_do_arquivo"]):
         if word.lower() in palavra.lower():
-            ocorrencias.append({ "linha": i + 1 })
+            ocorrencias.append({"linha": i + 1})
 
     if len(ocorrencias) != 0:
         result.append({
@@ -14,7 +14,7 @@ def exists_word(word, instance):
             "arquivo": data[0]["nome_do_arquivo"],
             "ocorrencias": ocorrencias
         })
-    
+
     return result
 
 
@@ -26,7 +26,7 @@ def search_by_word(word, instance):
 
     for i, palavra in enumerate(data[0]["linhas_do_arquivo"]):
         if word.lower() in palavra.lower():
-            ocorrencias.append({ "linha": i + 1, "conteudo": palavra })
+            ocorrencias.append({"linha": i + 1, "conteudo": palavra})
 
     if len(ocorrencias) != 0:
         result.append({
@@ -34,5 +34,5 @@ def search_by_word(word, instance):
             "arquivo": data[0]["nome_do_arquivo"],
             "ocorrencias": ocorrencias
         })
-    
+
     return result
